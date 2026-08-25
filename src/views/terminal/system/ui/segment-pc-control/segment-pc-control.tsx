16152339cfb8dd009ee45db9f5006a54dc23bf08
Navatusein <boghdan.kutsulima@gmail.com>
2026-08-25 09:54:38 +0300

    (Added) laptop power control button on terminal

--- src/views/terminal/system/ui/segment-pc-control/segment-pc-control.tsx
+++ src/views/terminal/system/ui/segment-pc-control/segment-pc-control.tsx
@@ -4,10 +4,12 @@ import {Grid} from "@/shared/ui-kit";
 import {ButtonButton} from "@/widgets/terminal-buttons/button-button";
 import {AgentDisabledProvider} from "@/features/agent-disabled-provider";
 import {PcDisabledProvider} from "@/features/pc-disabled-provider";
+import {ToggleButton} from "@/widgets/terminal-buttons/toggle-button";
 import PowerIcon from "@public/icons/devices-and-networks/power.svg";
 import LogoutIcon from "@public/icons/devices-and-networks/logout.svg";
 import ReloadIcon from "@public/icons/devices-and-networks/reload.svg";
-
+import DeviceLaptopIcon from "@public/icons/devices-and-networks/device-laptop.svg";
+import DeviceLaptopOffIcon from "@public/icons/devices-and-networks/device-laptop-off.svg";
 
 export default function SegmentPcControl() {
   return (
@@ -24,6 +26,16 @@ export default function SegmentPcControl() {
           <ButtonButton entity="button.navatusein_pc_restart_pc" icon={ReloadIcon}/>
         </Grid.Section>
       </PcDisabledProvider>
+
+      <Grid.Section rowStart={7} colStart={5} direction="horizontal">
+        <ToggleButton
+          entity="switch.laptop_nas_wr01b"
+          iconEnabled={DeviceLaptopIcon}
+          iconDisabled={DeviceLaptopOffIcon}
+          colorEnabled="blue"
+          title="Power"
+        />
+      </Grid.Section>
     </>
   )
 }
