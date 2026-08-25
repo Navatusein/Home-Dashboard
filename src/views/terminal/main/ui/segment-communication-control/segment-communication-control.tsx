af6a0d5afe9d8a451f8f8aab1bcb9472271836be
Navatusein <boghdan.kutsulima@gmail.com>
2026-08-25 09:54:19 +0300

    (Updated) hid teams control buttons on terminal

--- src/views/terminal/main/ui/segment-communication-control/segment-communication-control.tsx
+++ src/views/terminal/main/ui/segment-communication-control/segment-communication-control.tsx
@@ -2,26 +2,39 @@ import {Grid} from "@/shared/ui-kit";
 import {CommunicationButton} from "@/widgets/terminal-buttons/communication-button";
 import MicrophoneOff from "@public/icons/media/microphone-off.svg";
 import Microphone2Icon from "@public/icons/media/microphone-2.svg";
-import DeviceComputerCameraIcon from "@public/icons/streaming-and-socials/device-computer-camera.svg";
-import DeviceComputerCameraOffIcon from "@public/icons/streaming-and-socials/device-computer-camera-off.svg";
+// import DeviceComputerCameraIcon from "@public/icons/streaming-and-socials/device-computer-camera.svg";
+// import DeviceComputerCameraOffIcon from "@public/icons/streaming-and-socials/device-computer-camera-off.svg";
 import HeadphonesOffIcon from "@public/icons/media/headphones-off.svg";
 import HeadphonesIcon from "@public/icons/media/headphones.svg";
 
 export default function SegmentCommunicationControl() {
   return (
     <>
-      <Grid.Section rowStart={2} colStart={11} direction="horizontal">
-        <CommunicationButton
-          title="Teams"
-          switchEntity="switch.navatusein_pc_teams_mute"
-          isMeetingBinarySensor="binary_sensor.navatusein_pc_teams_meeting"
-          isApplicationActiveBinarySensor="binary_sensor.navatusein_pc_teams_plugin_status"
-          disabledIcon={Microphone2Icon}
-          iconOn={MicrophoneOff}
-          iconOff={Microphone2Icon}
-          colorOn="blue"
-          colorOff="default"
-        />
+      {/*<Grid.Section rowStart={2} colStart={11} direction="vertical">*/}
+      {/*  <CommunicationButton*/}
+      {/*    title="Teams"*/}
+      {/*    switchEntity="switch.navatusein_pc_teams_mute"*/}
+      {/*    isMeetingBinarySensor="binary_sensor.navatusein_pc_teams_meeting"*/}
+      {/*    isApplicationActiveBinarySensor="binary_sensor.navatusein_pc_teams_plugin_status"*/}
+      {/*    disabledIcon={Microphone2Icon}*/}
+      {/*    iconOn={MicrophoneOff}*/}
+      {/*    iconOff={Microphone2Icon}*/}
+      {/*    colorOn="blue"*/}
+      {/*    colorOff="default"*/}
+      {/*  />*/}
+      {/*  <CommunicationButton*/}
+      {/*    title="Teams"*/}
+      {/*    switchEntity="switch.navatusein_pc_teams_camera"*/}
+      {/*    isMeetingBinarySensor="binary_sensor.navatusein_pc_teams_meeting"*/}
+      {/*    isApplicationActiveBinarySensor="binary_sensor.navatusein_pc_teams_plugin_status"*/}
+      {/*    disabledIcon={DeviceComputerCameraIcon}*/}
+      {/*    iconOn={DeviceComputerCameraIcon}*/}
+      {/*    iconOff={DeviceComputerCameraOffIcon}*/}
+      {/*    colorOn="blue"*/}
+      {/*    colorOff="default"*/}
+      {/*  />*/}
+      {/*</Grid.Section>*/}
+      <Grid.Section rowStart={2} colStart={12} direction="vertical">
         <CommunicationButton
           title="Discord"
           switchEntity="switch.navatusein_pc_discord_mute"
@@ -30,19 +43,6 @@ export default function SegmentCommunicationControl() {
           disabledIcon={Microphone2Icon}
           iconOn={MicrophoneOff}
           iconOff={Microphone2Icon}
-          colorOn="blue"
-          colorOff="default"
-        />
-      </Grid.Section>
-      <Grid.Section rowStart={3} colStart={11} direction="horizontal">
-        <CommunicationButton
-          title="Teams"
-          switchEntity="switch.navatusein_pc_teams_camera"
-          isMeetingBinarySensor="binary_sensor.navatusein_pc_teams_meeting"
-          isApplicationActiveBinarySensor="binary_sensor.navatusein_pc_teams_plugin_status"
-          disabledIcon={DeviceComputerCameraIcon}
-          iconOn={DeviceComputerCameraIcon}
-          iconOff={DeviceComputerCameraOffIcon}
           colorOn="blue"
           colorOff="default"
         />
